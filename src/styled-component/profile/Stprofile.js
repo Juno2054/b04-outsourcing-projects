c183416e7b0e1940e2ba33cc30a0320cdccd9cba
strawberrysa <sapark0418@gmail.com>
2023-12-07 22:34:37 +0900

    마이페이지

--- src/styled-component/profile/Stprofile.js
+++ src/styled-component/profile/Stprofile.js
@@ -1,22 +1,19 @@
 import styled from 'styled-components'
 
 export const MyPageContainer = styled.div`
-  max-width: 70%;
+  width: 70%;
   margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
   border-radius: 8px;
   text-align: center;
-  margin-top: 50px;
   position: relative;
+  margin-top: 20px;
 `
 
 export const UserInfo = styled.div`
-  margin-bottom: 20px;
-  display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: nowrap;
   align-items: center;
+  border: 1px soli;
 `
 
 export const ProfileImage = styled.img`
@@ -42,13 +39,37 @@ export const Edit = styled.img`
   top: 20px;
   right: 20px;
 `
+
+/* 모달 스타일 */
 export const Modal = styled.div`
-  /* 모달 스타일 */
-  width: 70%;
-  background-color: #ffff;
+  width: 100%;
   border-radius: 8px;
   padding: 20px;
   box-shadow: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    width: 100%;
+    font-size: 12px;
+    text-align: left;
+    padding: 10px;
+    border-bottom: 2px solid #ccc;
+  }
+`
+export const ProfileWrap = styled.div`
+  position: relative;
+`
+
+export const ProfileEdit = styled.img`
+  width: 20px;
+  height: 20px;
+  object-fit: cover;
+  cursor: pointer;
+  position: absolute;
+  top: 50px;
+  right: 50px;
 `
 
 export const ModalOverlay = styled.div`
@@ -74,7 +95,7 @@ export const ModalContent = styled.div`
 export const EditButton = styled.button`
   background-color: #ea3267;
   color: white;
-  width: 70px;
+  width: 100%;
   height: 30px;
   border-radius: 10px;
   font-size: ${({ theme }) => theme.fontSize.sm};
@@ -116,7 +137,6 @@ export const Bio = styled.span`
   margin-top: 10px;
 `
 
-export const ModalInput = styled.div`
-  display: flex;
-  width: 80%;
+export const ModalInput = styled.input`
+  border: none;
 `
